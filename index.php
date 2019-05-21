ea23adf9921702b386ec1b87450fde9bc590f782
Darith-Mokpost <darith0001@gmail.com>
2019-05-21 10:47:53 +0700

    add function min max

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
 	<title>Find Min & Max From Array</title>
 </head>
 <body>
+	
 	<?php
+		$arr = Array(1,2,3,4,5,6,7,8,9,10);
 
-
+		$min=$arr[0];
+		$max=$min;
+		foreach($arr as $val)
+		{
+			if($min>$val)
+				$min=$val;
+			if($max<$val)
+				$max=$val;
+		}
+		echo "Min of array : ".$min."<br>"."Max of array : ".$max;
 	?>
 
 </body>
